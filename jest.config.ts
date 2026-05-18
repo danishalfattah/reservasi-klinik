acd696aadce503a561173daea0103debb1fed174
Danish Alfattah <danishalfattah25@gmail.com>
2026-05-18 18:45:35 +0700

    fix: ensure verbose output is enabled in Jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  verbose: true,
   roots: ['<rootDir>/tests'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
